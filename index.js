232947acb286d93924b9a21664dff8dcb9a756ef
kaelzhang <i@kael.me>
2013-10-19 17:24:04 +0800

    fix default gruntfile.js task

--- index.js
+++ index.js
@@ -25,18 +25,31 @@ function Hum (options) {
     this.cwd = options.cwd || process.cwd();
 
     this._config = [];
+    this._options = [];
+    this._tasks = [];
 };
 
+
+var fake_gruntfile = node_path.join(__dirname, 'lib', 'fake-gruntfile.js');
 
 Hum.prototype._run_grunt = function(err, done) {
     if ( err ) {
         return done(err);
     }
 
-    this._apply_config();
-
     var cli = grunt.cli;
-    grunt.tasks(cli.tasks, cli.options, done);
+
+    this._apply_config();
+    this._apply_options();
+    
+    cli.options.gruntfile = fake_gruntfile;
+
+    grunt.tasks(this._tasks, cli.options, done);
+};
+
+
+Hum.prototype._collect_tasks = function(args) {
+    this._tasks = this._tasks.concat(args);
 };
 
 
@@ -54,7 +67,21 @@ Hum.prototype._apply_config = function() {
 };
 
 
-Hum.prototype._load_npm_tasks = function(names) { console.log(this.path);
+Hum.prototype._collect_options = function(options) {
+    this._options = this._options.concat(options);
+};
+
+
+Hum.prototype._apply_options = function() {
+    var grunt_options = grunt.cli.options;
+
+    this._options.forEach(function (options) {
+        util.mix(grunt_options, options)
+    });
+};
+
+
+Hum.prototype._load_npm_tasks = function(names) {
     names.forEach(this._load_npm_task, this);
 };
 
@@ -67,7 +94,6 @@ Hum.prototype._load_npm_task = function(name) {
 
         path = node_path.join(path, name, 'tasks');
 
-console.log('path', path)
         if ( util.isDir(path) ) {
             grunt.task.loadTasks(path);
             return true;
@@ -81,18 +107,14 @@ Hum.prototype._register_multi_task = function(tasks) {
 };
 
 
-Hum.prototype._register_tasks = function(args) {
-    grunt.registerTask.apply(null, args);
-};
-
-
 deferrer({
     host: Hum.prototype,
     type: 'parallel'
 })
 .promise('config', '_collect_config')
 .promise('npmTasks', '_load_npm_tasks')
-.promise('task', '_register_tasks')
+.promise('task', '_collect_tasks')
 .promise('multiTask', '_register_multi_task')
+.promise('options', '_collect_options')
 .done('_run_grunt');
 
